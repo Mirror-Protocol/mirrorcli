9a84bec99065422367225de7558d9c49df0beadd
William Chen <william@terra.money>
2020-11-10 20:51:40 +0900

    add config

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,102 @@ import * as path from 'path';
 
 import { Validator } from 'jsonschema';
 const { error } = require('prettycli');
+import * as _ from 'lodash';
 
 export namespace MirrorCLIConfig {
   export const SCHEMA = {
-    type: 'object',
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    $ref: '#/definitions/MirrorCLIConfig',
+    definitions: {
+      ContractConfig: {
+        title: 'ContractConfig',
+        type: 'object',
+        properties: {
+          contractAddress: {
+            type: 'string',
+            title: 'contractAddress',
+          },
+          codeId: {
+            type: 'number',
+            title: 'codeId',
+          },
+        },
+      },
+      MirrorCLIConfig: {
+        title: 'MirrorCLIConfig',
+        type: 'object',
+        properties: {
+          lcd: {
+            type: 'object',
+            properties: {
+              chainId: {
+                type: 'string',
+                title: 'chainId',
+              },
+              url: {
+                type: 'string',
+                title: 'url',
+              },
+            },
+            title: 'lcd',
+          },
+          contracts: {
+            type: 'object',
+            properties: {
+              mirror: {
+                type: 'object',
+                properties: {
+                  collector: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'collector',
+                  },
+                  factory: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'factory',
+                  },
+                  gov: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'gov',
+                  },
+                  mint: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'mint',
+                  },
+                  oracle: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'oracle',
+                  },
+                  staking: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'staking',
+                  },
+                },
+                title: 'mirror',
+              },
+              terraswap: {
+                type: 'object',
+                properties: {
+                  factory: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'factory',
+                  },
+                  pair: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'pair',
+                  },
+                  token: {
+                    $ref: '#/definitions/ContractConfig',
+                    title: 'token',
+                  },
+                },
+                title: 'terraswap',
+              },
+            },
+            title: 'contracts',
+          },
+        },
+      },
+    },
   };
 }
 
@@ -16,12 +108,17 @@ export interface ContractConfig {
   codeId: number;
 }
 
+const defaultContractConfig = {
+  contractAddress: '',
+  codeId: 0,
+};
+
 export interface MirrorCLIConfig {
-  lcdConfig: {
+  lcd: {
     chainId: string;
     url: string;
   };
-  contracts?: {
+  contracts: {
     mirror: {
       collector: ContractConfig;
       factory: ContractConfig;
@@ -41,9 +138,24 @@ export interface MirrorCLIConfig {
 export const configFilePath = path.join(homedir(), '.mirrorclirc.json');
 
 export const DEFAULT_MIRRORCLI_CONFIG: Partial<MirrorCLIConfig> = {
-  lcdConfig: {
+  lcd: {
     chainId: 'columbus-4',
     url: 'https://lcd.terra.dev',
+  },
+  contracts: {
+    mirror: {
+      collector: defaultContractConfig,
+      factory: defaultContractConfig,
+      gov: defaultContractConfig,
+      mint: defaultContractConfig,
+      oracle: defaultContractConfig,
+      staking: defaultContractConfig,
+    },
+    terraswap: {
+      factory: defaultContractConfig,
+      pair: defaultContractConfig,
+      token: defaultContractConfig,
+    },
   },
 };
 
@@ -51,13 +163,9 @@ export function validateConfig(
   config: Partial<MirrorCLIConfig>
 ): Partial<MirrorCLIConfig> {
   const v = new Validator();
-  const data = {
-    ...DEFAULT_MIRRORCLI_CONFIG,
-    ...config,
-  };
-  const r = v.validate(data, MirrorCLIConfig.SCHEMA);
+  const r = v.validate(config, MirrorCLIConfig.SCHEMA);
   if (r.valid) {
-    return data;
+    return config;
   } else {
     for (const err of r.errors) {
       console.error(err.toString());
@@ -82,9 +190,6 @@ export function loadConfig(): Partial<MirrorCLIConfig> {
     const loadedConfig = JSON.parse(fs.readFileSync(configFilePath).toString());
     // first, perform validation & reformat
     saveConfig(loadedConfig);
-    return {
-      ...DEFAULT_MIRRORCLI_CONFIG,
-      ...loadedConfig,
-    };
+    return _.merge(DEFAULT_MIRRORCLI_CONFIG, loadedConfig);
   }
 }
