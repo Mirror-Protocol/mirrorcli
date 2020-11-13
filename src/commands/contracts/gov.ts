a5b7f88b44755a7a5fee4e3406ab69b0fce9bd6e
William Chen <william@terra.money>
2020-11-13 18:18:33 +0900

    (WIP) gov

--- src/commands/contracts/gov.ts
+++ src/commands/contracts/gov.ts
@@ -1,12 +1,61 @@
 import { Command } from 'commander';
 
-const exec = new Command('collector');
-exec.description('Mirror Gov contract functions');
-exec.command('open-position');
+import { Parse } from '../../util/parse-input';
+import {
+  createExecMenu,
+  handleExecCommand,
+  handleQueryCommand,
+} from '../../util/contract-menu';
+
+const exec = createExecMenu('gov', 'Mirror Gov contract functions');
+
+const castVote = exec
+  .command('cast-vote <poll-id> <vote> <amount>')
+  .description(`Vote in an active poll`, {
+    'poll-id': '(int) Poll ID',
+    vote: `(string) 'yes' or 'no'`,
+    amount: '(Uint128) amount of staked MIR voting power to allocate',
+  })
+  .action((pollId: string, vote: string, amount: string) => {
+    if (vote !== 'yes' && vote !== 'no') {
+      throw new Error(`invalid vote option '${vote}', MUST be 'yes' or 'no'`);
+    }
+    handleExecCommand(exec, mirror =>
+      mirror.gov.castVote(Parse.int(pollId), vote, amount)
+    );
+  });
+
+const castVote = exec
+  .command('cast-vote <poll-id> <vote> <amount>')
+  .description(`Vote in an active poll`, {
+    'poll-id': '(int) Poll ID',
+    vote: `(string) 'yes' or 'no'`,
+    amount: '(Uint128) amount of staked MIR voting power to allocate',
+  })
+  .action((pollId: string, vote: string, amount: string) => {
+    if (vote !== 'yes' && vote !== 'no') {
+      throw new Error(`invalid vote option '${vote}', MUST be 'yes' or 'no'`);
+    }
+    handleExecCommand(exec, mirror => mirror.gov.createPoll());
+  });
+
+const send = exec
+  .command('send')
+  .description(
+    `Sends contract's balance of MIR to distribution (Mirror Gov) contract`
+  )
+  .action(() => {
+    handleExecCommand(exec, mirror => mirror.collector.send());
+  });
 
 const query = new Command('collector');
-query.description('Mirror Gov contract queries');
-query.command('config');
+query.description('Mirror Collector contract queries');
+const getConfig = query
+  .command('config')
+  .description('Query Mirror Collector contract config')
+  .action(() => {
+    handleQueryCommand(query, mirror => mirror.collector.getConfig());
+  });
 
 export default {
   exec,
--- src/commands/contracts/staking.ts
+++ src/commands/contracts/staking.ts
@@ -1,12 +1,118 @@
 import { Command } from 'commander';
 
-const exec = new Command('staking');
-exec.description('Mirror Staking contract functions');
-exec.command('open-position');
+import { Parse } from '../../util/parse-input';
+import {
+  createExecMenu,
+  handleExecCommand,
+  handleQueryCommand,
+} from '../../util/contract-menu';
+import { TerraswapToken } from '@mirror-protocol/mirror.js';
+
+const exec = createExecMenu('staking', 'Mirror Staking contract functions');
+
+const registerAsset = exec
+  .command('register-asset <asset-token> <staking-token>')
+  .description('Register asset with Mirror Staking contract', {
+    'asset-token': '(AccAddress) mAsset or MIR token',
+    'staking-token': '(AccAddress) Associated LP token',
+  })
+  .action((assetToken: string, stakingToken: string) => {
+    handleExecCommand(exec, mirror =>
+      mirror.staking.registerAsset(assetToken, stakingToken)
+    );
+  });
+
+const updateConfig = exec
+  .command('update-config')
+  .option('--owner <string>', 'New owner address')
+  .description('Update Mirror Staking contract config')
+  .action(() => {
+    handleExecCommand(exec, mirror =>
+      mirror.staking.updateConfig({
+        owner: updateConfig.owner,
+      })
+    );
+  });
+
+const bond = exec
+  .command('bond <asset-token> <amount>')
+  .description('Stakes LP token for asset', {
+    'asset-token': '(AccAddress) mAsset or MIR token',
+    amount: '(Uint128) amount of LP token to bond',
+  })
+  .action((assetToken: string, amount: string) => {
+    handleExecCommand(exec, mirror =>
+      mirror.staking.bond(assetToken, amount, new TerraswapToken({}))
+    );
+  });
+
+const withdraw = exec
+  .command('withdraw <asset-token>')
+  .description('Withdraw asset from Mirror Staking contract', {
+    'asset-token': '(AccAddress) mAsset or MIR token',
+  })
+  .action((assetToken: string) => {
+    handleExecCommand(exec, mirror => mirror.staking.withdraw(assetToken));
+  });
+
+const depositReward = exec
+  .command('deposit-reward <asset-token>')
+  .description(`Adds MIR tokens to the rewards for the asset's staking pool`, {
+    'asset-token': '(AccAddress) address of staking pool to reward',
+    amount: '(Uint128) amount of MIR token to deposit',
+  })
+  .action((assetToken: string, amount: string) => {
+    handleExecCommand(exec, mirror => {
+      return mirror.staking.depositReward(
+        assetToken,
+        amount,
+        mirror.mirrorToken
+      );
+    });
+  });
+
+const unbond = exec
+  .command('unbond <asset-token> <amount>')
+  .description(`Unstakes LP tokens from staking pool`, {
+    'asset-token': '(AccAddress) mAsset or MIR token',
+    amount: '(Uint128) amount of LP tokens to unbond',
+  })
+  .action((assetToken: string, amount: string) => {
+    handleExecCommand(exec, mirror =>
+      mirror.staking.unbond(assetToken, amount)
+    );
+  });
 
 const query = new Command('staking');
-query.description('Mirror Staking contract queries');
-query.command('config');
+query.description('Mirror staking contract queries');
+const getConfig = query
+  .command('config')
+  .description('Query Mirror Staking contract config')
+  .action(() => {
+    handleQueryCommand(query, mirror => mirror.staking.getConfig());
+  });
+
+const getPoolInfo = query
+  .command('pool-info <asset-token>')
+  .description('Query Mirror Staking pool info', {
+    'asset-token': '(AccAddress) mAsset or MIR token',
+  })
+  .action((assetToken: string) => {
+    handleQueryCommand(query, mirror => mirror.staking.getPoolInfo(assetToken));
+  });
+
+const getRewardInfo = query
+  .command('reward-info <staker> [asset-token]')
+  .description('Query reward info for staker', {
+    staker: '(AccAddress) staker for whom to query rewards',
+    'asset-token':
+      '(AccAddress) mAsset or MIR token. If empty, returns all rewards.',
+  })
+  .action((staker: string, assetToken?: string) => {
+    handleQueryCommand(query, mirror =>
+      mirror.staking.getRewardInfo(staker, assetToken)
+    );
+  });
 
 export default {
   exec,
