bbe44651bb0e73446aa29a6e056bb875471b548b
William Chen <wchen298@gmail.com>
2020-11-14 10:06:10 +0900

    (WIP) terraswap factory

--- src/commands/contracts/terraswap.ts
+++ src/commands/contracts/terraswap.ts
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
+import * as fs from 'fs';
 
 import { Parse } from '../../util/parse-input';
 import {
@@ -6,19 +7,45 @@ import {
   handleExecCommand,
   handleQueryCommand,
 } from '../../util/contract-menu';
+import { TerraswapFactory } from '@mirror-protocol/mirror.js';
 
 const exec = createExecMenu(
   'terraswap',
   'Terraswap Factory contract functions'
 );
 
-const convert = exec
-  .command('convert <asset-token>')
-  .description(`Convert contract's balance of asset to UST or MIR`, {
-    'asset-token': '(AccAddress) mAsset or MIR token',
-  })
-  .action((assetToken: string) => {
-    handleExecCommand(exec, mirror => mirror.terraswapFactory.createPair());
+const updateConfig = exec
+  .command('update-config')
+  .description(`Update Terraswap Factory contract config`)
+  .option('--owner <string>', 'New contract owner')
+  .option('--pair-code-id <int>', 'New pair code ID')
+  .option('--token-code-id <int>', 'New token code ID')
+  .action(() => {
+    handleExecCommand(exec, mirror =>
+      mirror.terraswapFactory.updateConfig({
+        owner: updateConfig.owner,
+        pair_code_id: updateConfig.pairCodeId,
+        token_code_id: updateConfig.tokenCodeId,
+      })
+    );
+  });
+
+const createPair = exec
+  .command('create-pair <asset1> <asset2>')
+  .description(`Create new Terraswap pair`)
+  .option('--hook <path>', 'File containing hook message to attach')
+  .action((asset1: string, asset2: string) => {
+    let hook: any;
+    if (createPair.hook) {
+      hook = JSON.parse(fs.readFileSync(createPair.hook).toString());
+    }
+
+    handleExecCommand(exec, mirror =>
+      mirror.terraswapFactory.createPair(
+        [Parse.assetInfo(asset1), Parse.assetInfo(asset2)],
+        hook as TerraswapFactory.InitHook
+      )
+    );
   });
 
 const send = exec
